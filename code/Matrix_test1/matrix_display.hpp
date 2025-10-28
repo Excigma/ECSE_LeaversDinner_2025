996bac0bc5ed15229bd2f3d52f94e8afba01737f
Excigma <git@excigma.xyz>
2025-10-29 00:27:56 +1300

    feat(firmware): change brightness depending on internal temperature

--- code/Matrix_test1/matrix_display.hpp
+++ code/Matrix_test1/matrix_display.hpp
@@ -4,7 +4,7 @@
 #include <pico/stdlib.h>
 extern uint8_t scroll_buff[15];
 const uint8_t* char_to_matrix(const char charIn);
-void disp_char(const uint8_t * character);
+void disp_char(const uint8_t * character, float brightness);
 void scroll_chars(void);
 void add_char_to_scroll(const uint8_t * character);
 void add_char_to_scroll_start(const uint8_t * character);
